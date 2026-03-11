2a0d600c7a55d78194b6665e33b7702f7d682222
gangadhar23893 <gangadhar23893@gmail.com>
2026-03-11 01:04:17 +0000

    data validation done

--- CosmeticDetection/constant/training_pipeline/__init__.py
+++ CosmeticDetection/constant/training_pipeline/__init__.py
@@ -7,3 +7,13 @@ Data ingestion related constant start with DATA_INGESTION_VAR_NAME
 DATA_INGESTION_DIR_NAME : str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR :str = "feature_store"
 DATA_DOWNLOAD_URL : str ="https://drive.google.com/file/d/1DVbUpp3V_YCVlXB_KylsniCE3xbWxSsf/view?usp=sharing"
+
+
+"""
+Data validation related constant start with DATA_VALIDATION_VAR_NAME
+"""
+
+DATA_VALIDATION_DIR_NAME:str = "data_validation"
+DATA_VALIDATION_STATUS_FILE:str = "status.txt"
+DAT_VALIDATION_ALL_REQUIRED_FILES = ["train","valid","data.yaml"]
+
--- CosmeticDetection/pipeline/training_pipeline.py
+++ CosmeticDetection/pipeline/training_pipeline.py
@@ -3,12 +3,14 @@ import sys
 from CosmeticDetection.logger import logging
 from CosmeticDetection.exception import AppException
 from CosmeticDetection.components.data_ingestion import DataIngestion
-from CosmeticDetection.entity.config_entity import DataIngestionConfig
-from CosmeticDetection.entity.artifacts_entity import DataIngestionArtifact
+from CosmeticDetection.components.data_valition import DataValidation
+from CosmeticDetection.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from CosmeticDetection.entity.artifacts_entity import DataIngestionArtifact,DataValidationArtifact
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
     def start_data_ingestion(self) -> DataIngestionArtifact:
         try:
             logging.info("entered data ingestion method of training pipeline")
@@ -22,10 +24,20 @@ class TrainPipeline:
             return data_ingestion_artifact
         except Exception as e:
             raise AppException(e,sys)
+    
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact)->DataIngestionArtifact:
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,data_validation_config=self.data_validation_config)
+            data_validation_artifact = data_validation.initiate_data_validation()
+            logging.info("Performed data validation operations")
+            return data_validation_artifact
+        except Exception as e:
+            raise AppException(e,sys)
         
     def run_pipeline(self)->None:
         try:
             data_ingestion_artifact=self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
         except Exception  as e:
             raise AppException(e,sys)
         
