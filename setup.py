fa9f9c7953d6d4511a438be9b360aa61dfd44d6b
gangadhar23893 <gangadhar23893@gmail.com>
2026-04-05 03:41:27 +0000

    requirements.added and environmet created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "CosmeticDetection",
+    version = "0.0.0",
+    author = "Gangadhar Thatavarthi",
+    author_email="gangadhar23893@gmail.com",
+    packages = find_packages(),
+    install_requires=[]
+)
